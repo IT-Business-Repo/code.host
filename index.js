59bcadd4bac5499243ddb61a28ace09f5caa3c8b
IT-Business-Repo <67789561+IT-Business-Repo@users.noreply.github.com>
2021-03-29 19:16:45 +0000

    IT BUSINESS SITE JS

---
+++ index.js
@@ -0,0 +1,214 @@
+
+
+let urls = new Map();
+
+urls.set('goya', 'https://goya-template.webflow.io/');
+urls.set('entrepreneur', 'http://entrepreneurtemplate.webflow.io/');
+urls.set('entrepreneur-img', [
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/600650d8f544612c3fb677cc_Entrepreneur%20Template%20-%201.png',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/600650d7dcd32f19c3ae830b_Entrepreneur%20Template%20-%202.png',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/600650d7d1957aac5527b28d_Entrepreneur%20Template%20-%203.png',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/600650d846f5691a2d09c2be_Entrepreneur%20Template%20-%204.png',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/600650d756cb3d04038a8f24_Entrepreneur%20Template%20-%205.png'
+]);
+urls.set('darkly', 'https://darkly.webflow.io');
+urls.set('darkly-img', [
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f3fd0201694ae2c0cd556ca_gallery-1-p-1080.jpeg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f3fd020e4708ad6ef3b02ec_gallery-2-p-1080.jpeg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f3fd0204187655cf843ec2b_gallery-3-p-1080.jpeg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f3fd0207f83395734203d88_gallery-4-p-1080.jpeg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f3fd02014bb6a2c7230b774_gallery-5-p-1080.jpeg'
+]);
+urls.set('faraday', 'http://faraday-template.webflow.io/');
+urls.set('faraday-img', [
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/605ad501a6b1312acab311a8_faraday-gallery-01.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/60515e4da14ed3dc405ce3c3_faraday-gallery-02.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/60515e4ea6c6fd5715d7181b_faraday-gallery-03.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/60515e4e37eadaea9397a6eb_faraday-gallery-04.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/60515e4da14ed366235ce3c2_faraday-gallery-05.jpg'
+]);
+urls.set('iris', 'https://iris-template.webflow.io/');
+urls.set('iris-img', [
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fc566cdabeb394d52913488_gallery_1.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fc566cd59cf042306c00f1d_gallery_2.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fc566ce142847b9e822c239_gallery_3.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fc566ce6a126d0ab14b0854_gallery_4.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fc566ce023b240effa1154c_gallery_5.jpg'
+]);
+urls.set('scarpa', 'https://scarpa.webflow.io/');
+urls.set('scarpa-img', [
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fae56dafcd015106f7d2236_Gallery-Image-1.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fae56dfac2af94c3c1339a1_Gallery-Image-2.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fae56dfbab221c3d65f46b2_Gallery-Image-3.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fae56df7783b6669742ee3e_Gallery-Image-4.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fae56e369f9720a057bc262_Gallery-Image-%205.jpg'
+]);
+urls.set('galatea', 'https://galatea.webflow.io/');
+urls.set('galatea-img', [
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fac1105517b1426e936ed85_gallery_1.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fac110612793d725c160e3e_gallery_2.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fac11069ac7cab145d4a63d_gallery_3.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fac1104e2c2cef36bf8bde6_gallery_4.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5fac110452f2ac7d54aa5820_gallery_5.jpg'
+]);
+urls.set('apico', 'https://apico-template.webflow.io/');
+urls.set('apico-img', [
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5e9ccddfd3886a6b4cf7c358_5e98fc83531fea7146008193_Template-Slideshow.jpeg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5e9ccddfd3886a9c34f7c357_5e98fd16edae5d98b5543982_Template-Slideshow-2.jpeg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5e9ccddfd3886a02e5f7c359_5e98fd3b531fea853a0088b4_Template-Slideshow-3.jpeg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5e9ccddfd3886a608ff7c35b_5e98fd4ed0f2eb0316088ea2_Template-Slideshow-4.jpeg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5e9ccddfd3886aa196f7c35a_5e98fd5e85ef98bd4f737bf0_Template-Slideshow-5.jpeg'
+]);
+urls.set('cosmo', 'https://cosmo-template.webflow.io/');
+urls.set('cosmo-img', [
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5eed3fe7fb8f07f5b9f1081e_Template-Slideshow.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5eed3fe70f498bc8adf662ab_Template-Slideshow-2.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5eed3fe7d5fb56649d0899f8_Template-Slideshow-3.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5eed3fe7e9cf27d96ce562bf_Template-Slideshow-4.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5eed3fe7be3ccd0966aa0c07_Template-Slideshow-5.jpg'
+]);
+urls.set('otis', 'https://otis-template.webflow.io/');
+urls.set('otis-img', [
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f15e387e10b342bea6840c2_Template-Slideshow.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f15e3867ba40652abbea273_Template-Slideshow-2.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f15e386103a8901e3446fc2_Template-Slideshow-3.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f15e38665c7c4d373094061_Template-Slideshow-4.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f15e387364a4b42136f92f0_Template-Slideshow-5.jpg'
+]);
+
+urls.set('tara', 'https://tara-portfolio.webflow.io/');
+urls.set('tara-img', [
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5ff8d3c79f2f4c11b751eaf2_1.png',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5ff8d3c7d1eabc7b1ab04df1_2.png',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5ff8d3c6fc2054afd31d320c_3.png',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5ff8d3c64d19bf207acced71_4.png',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5ff8d3c63da7627d597bc116_5.png'
+]);
+
+urls.set('collective', 'http://collective-template.webflow.io/');
+urls.set('collective-img', [
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f21e7466a43cf14bbe5e368_gallery-01.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f21e74626d41df3a50295cd_gallery-02.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f21e746dc1e151c4cb1c414_gallery-03.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f21e746ed6fc6ef893009da_gallery-04.jpg',
+    'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5f21e7463061b677900a97c4_gallery-05.jpg'
+]);
+
+urls.set('meko', 'https://meko-template.webflow.io/');
+urls.set('meko-img', [
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/6048191c6cf41483917a979c_Gallery%201.png',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/6048191b04c2027d6cbfbd24_Gallery%202.png',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/6048191b45928f892ca8ea88_Gallery%203.png',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/6048191b0ee0b06055941638_Gallery%204.png',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/6048191b8cb020921fa1e4a8_Gallery%205.png'
+] );
+
+urls.set('code', 'https://codetemplate.webflow.io/');
+urls.set('code-img', [
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/60302d2ccb9501160a48240e_Code%20Template%20-%201.png',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/60302d2c71aa6a3e81d249ee_Code%20Template%20-%202.png',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/60302d2cbcc0b14144dbc399_Code%20Template%20-%203.png',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/60302d2ce2ceb1f50e9ff226_Code%20Template%20-%204.png',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/60302d2cff2048bb513a4165_Code%20Template%20-%205.png'
+] );
+
+
+urls.set('dawn', 'https://dawn-dashboard.webflow.io/');
+urls.set('dawn-img', [
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/6046bbc4f8122abf9a7f4c30_Dawn%20Dashboard%20Screenshot%201.jpg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/6046bbc446f7a19233f9db28_Dawn%20Dashboard%20Screenshot%202.jpg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/6046bbc4a1bf27efcf240886_Dawn%20Dashboard%20Screenshot%203.jpg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/6046bbc4c3503a2f24a1329b_Dawn%20Dashboard%20Screenshot%204.jpg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/6046bbc4b6441d2897b13939_Dawn%20Dashboard%20Screenshot%205.jpg'
+] );
+
+urls.set('seaside', 'http://template-seaside.webflow.io/');
+urls.set('seaside-img', [
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5e593fb060cf8709f775ebcb_seaside-1%402x.jpg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5e5ddf18afa1fe45220f0988_58531d7356ce728d0ed6049e_seaside-2%25402x.jpeg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5e5ddf18afa1fe30780f0989_58531d7356ce728d0ed6030c_seaside-3%25402x.jpeg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5e5ddf18afa1fe7b250f0987_58531d7356ce728d0ed60355_seaside-4%25402x.jpeg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/5e5ddf18afa1fe30780f0989_58531d7356ce728d0ed6030c_seaside-3%25402x.jpeg'
+] );
+
+urls.set('grandhotel', 'https://grand-hotel.webflow.io/');
+urls.set('grandhotel-img', [
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/605fbc30150ebe16e0e7409f_74a209eab72e4659acc5d8a7725dcd0e.jpeg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/605fbc30150ebe974de740a0_3a65a0b1932149c19ec70efdd0a00231.jpeg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/605fbc30150ebe8410e740a1_214216558a3c4eb8955398ce4989bc2a.jpeg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/605fbc30150ebea8aae7409d_6cf72720b3b34ac29caf90b0cdd5aeb9.jpeg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/605fbc30150ebe3867e740a2_929d508926954d6587d35e95819075f2.jpeg'
+] );
+
+urls.set('photographa', 'https://photographa.webflow.io/');
+urls.set('photographa-img', [
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/604c01db5e9888faa77c3017_1%20Slider%20Image%201440x900.jpg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/604c01db2582aa1487e2a3d5_2%20Slider%20Image%201440x900.jpg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/604c01db2582aadadce2a3d6_3%20Slider%20Image%201440x900.jpg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/604c01dbd633695bf4cda082_4%20Slider%20Image%201440x900.jpg',
+'https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/604c01db4ab0c855d4e58ad4_5%20Slider%20Image%201440x900.jpg'
+] );
+
+
+
+var app = new Vue({
+    el: '#proddesc',
+    data: {
+        titre: '',
+        href: '',
+        getImg: [""]
+    },
+    created: function () {
+        var currentUrl = window.location.pathname;
+        var split = currentUrl.split('/');
+        let length = split.length - 1;
+        var name = split[length];
+        // alert(name);
+        let capitalise = name.charAt(0).toUpperCase() + name.slice(1);
+        this.titre = capitalise;
+
+
+        this.href = urls.get(name);
+        this.getImg = urls.get(name + '-img');
+
+        var x = document.getElementById("slide-1");
+        x.setAttribute("srcset", this.getImg[0]);
+
+        var x2 = document.getElementById("slide-2");
+        x2.setAttribute("srcset", this.getImg[1]);
+
+        var x3 = document.getElementById("slide-3");
+        x3.setAttribute("srcset", this.getImg[2]);
+
+        var x4 = document.getElementById("slide-4");
+        x4.setAttribute("srcset", this.getImg[3]);
+
+        var x5 = document.getElementById("slide-5");
+        x5.setAttribute("srcset", this.getImg[4]);
+        //x[0].src = this.getImg[0];
+        //alert(this.getImg);
+    },
+    methods: {
+        getProductName: function () {
+            return this.message.split('').reverse().join('')
+        }
+    }
+
+})
+
+var app = new Vue({
+    el: '#temp-detail',
+    data: {
+        titre: '',
+    },
+    created: function () {
+        var currentUrl = window.location.pathname;
+        var split = currentUrl.split('/');
+        let length = split.length - 1;
+        var name = split[length];
+        // alert(name);
+        let capitalise = name.charAt(0).toUpperCase() + name.slice(1);
+        this.titre = capitalise;
+    }
+})
+
